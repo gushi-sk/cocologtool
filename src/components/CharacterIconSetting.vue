1b4a14c30c2b10cc49c0d65973af3a5642757dcc
ShoheiKasai <sheep.got.wings0923@gmail.com>
2025-07-13 14:23:04 +0900

    キャラクターアイコン設定画面のレイアウトを改善し、各キャラクターのアイコン表示を整理しました。色設定機能のスタイルを調整し、ファイルアップロードのレイアウトを整えました。

--- src/components/CharacterIconSetting.vue
+++ src/components/CharacterIconSetting.vue
@@ -3,15 +3,62 @@
     <h2>キャラクターアイコン画像設定</h2>
     <button @click="emitBack" style="margin-left:1em;">ログアップロード画面に戻る</button>
     <div style="margin-bottom: 32px;"></div>
-    <div v-for="char in localCharacters" :key="char.name" style="display:flex;align-items:center;margin-bottom:1em">
-      <div style="width:60px;height:60px;background:#000;display:flex;align-items:center;justify-content:center;margin-right:1em;">
-        <img v-if="icons[char.name]" :src="icons[char.name]" style="width:100%;height:100%;object-fit:cover;" />
+    <div v-for="char in localCharacters" :key="char.name" class="char-row">
+      <div class="char-col icon-col">
+        <div class="icon-box">
+          <img v-if="icons[char.name]" :src="icons[char.name]" />
+        </div>
       </div>
-      <span :style="{color: char.color, fontWeight:'bold', marginRight:'1em'}">{{ char.name }}</span>
-      <input type="color" v-model="char.color" style="margin-right:0.5em;" />
-      <input type="text" v-model="char.color" style="width:90px;margin-right:1em;" />
-      <input type="file" accept="image/*" @change="e=>onIconChange(e, char.name)" />
+      <div class="char-col name-col">
+        <span :style="{color: char.color, fontWeight:'bold'}">{{ char.name }}</span>
+      </div>
+      <div class="char-col fontcolor-col">
+        <input type="color" v-model="char.color" style="margin-right:0.5em;" />
+        <input type="text" v-model="char.color" style="width:90px;" />
+      </div>
+      <div class="char-col file-col">
+        <input type="file" accept="image/*" @change="e=>onIconChange(e, char.name)" />
+      </div>
     </div>
+/* キャラクター設定行のレイアウト調整 */
+.char-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1em;
+  gap: 0.5em;
+}
+.char-col {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.icon-col {
+  width: 60px;
+}
+.icon-box {
+  width: 60px;
+  height: 60px;
+  background: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.icon-box img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.name-col {
+  min-width: 80px;
+  margin-right: 1em;
+}
+.fontcolor-col {
+  min-width: 160px;
+  margin-right: 1em;
+}
+.file-col {
+  min-width: 140px;
+}
     <button @click="emitDone">設定完了</button>
   </div>
 </template>
@@ -38,8 +85,10 @@ function onIconChange(e: Event, name: string) {
 function emitDone() {
   // 色変更も反映してemit
   emit('done', { icons: icons.value, characters: localCharacters.value })
+  window.scrollTo({ top: 0, behavior: 'auto' })
 }
 function emitBack() {
   emit('back')
+  window.scrollTo({ top: 0, behavior: 'auto' })
 }
 </script>
